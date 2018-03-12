e83dd4796fb154fbd7bea0b4e70e5aa8e9d2371b
LucileFloc <lucile.floc@gmail.com>
2018-03-12 18:07:58 +0100

    question 2 et 3

--- interaction.js
+++ interaction.js
@@ -2,13 +2,41 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	//Question 1 Définir ici les attributs de la 'classe'
+
+	//Définir ici les attributs de la 'classe'
+
     this.posInitX = 0;
     this.posInitY = 0;
     this.posFinalX = 0;
     this.posFinalY = 0;
+    this.estPresse = false;
 
-	// Developper les 3 fonctions gérant les événements
+	//Developper les 3 fonctions gérant les événements
+    //Implémentation
+
+    //Pression de la souris
+    this.pression = function(evt){
+        this.posInitX = getMousePosition(canvas, evt).x;
+        this.posInitY = getMousePosition(canvas, evt).y;
+        this.estPresse = true;
+    }.bind(this);
+
+    // Mouvement de la souris
+    this.deplacement = function(evt){
+        if (this.estPresse){
+            this.posFinalX = getMousePosition(canvas, evt).x;
+            this.posFinalY = getMousePosition(canvas, evt).y;
+        }
+    }.bind(this);
+
+    // Relâchement de la souris
+    this.relachement = function(evt){
+        if (this.estPresse){
+            this.posFinalX = getMousePosition(canvas, evt).x;
+            this.posFinalY = getMousePosition(canvas, evt).y;
+            this.estPresse = false;
+        }
+    }.bind(this);
 
 	// Associer les fonctions précédentes aux évènements du canvas.
 };
